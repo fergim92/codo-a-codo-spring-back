78674ecbe7db20521196cfe44896547888724f32
Fernando Gimenez <gimenezfernandomarcos@gmail.com>
2022-09-07 11:25:13 -0300

    Clase 3

---
+++ src/main/java/ar/com/codoacodo/Main.java
@@ -0,0 +1,17 @@
+package ar.com.codoacodo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		
+		Auto miPrimerAuto = new Auto();
+		
+		Auto miSegundoAuto;
+		miSegundoAuto = new Auto();
+		
+		miPrimerAuto.encender();
+
+	}
+
+}
