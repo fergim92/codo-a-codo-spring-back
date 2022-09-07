f9fc4453e436c5bbebde5e8194b93f8d0e2967e8
Fernando Gimenez <gimenezfernandomarcos@gmail.com>
2022-09-07 13:17:07 -0300

    clase 4

--- src/main/java/ar/com/codoacodo/Main.java
+++ src/main/java/ar/com/codoacodo/Main.java
@@ -2,16 +2,29 @@ package ar.com.codoacodo;
 
 public class Main {
 
+	//voy a darle play al programa
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+			
+		System.out.println("hola mundo");
+			
+		//voy a crear una instancia de la clase Auto
+				
+		Auto miPrimerAuto = new Auto();//nace mi auto
+		/*
+		 * Auto miPrimerAuto; defini un objeto(no nacio)
+		 * miPrimerAuto = new Auto(); nace
+		 * esta forma es util para usar en if si quiero que nazca con
+		 * determinado constructor, primero defino el objeto
+		 * y dependiendo la rama del if nace con ciertas caracteristicas
+		 */
 		
-		Auto miPrimerAuto = new Auto();
 		
-		Auto miSegundoAuto;
-		miSegundoAuto = new Auto();
-		
+		//aca ya puedo usar los metodos de ese auto	
+		miPrimerAuto.mostrarInfo();
+			
+		//ejecutar el metodo encender()
 		miPrimerAuto.encender();
-
+			
+		miPrimerAuto.mostrarInfo();
 	}
-
 }
